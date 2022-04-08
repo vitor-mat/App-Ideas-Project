97ab31aa430b9fd153a1dfc318270350a01ac96a
vitor-mat <vitormateusalves123@gmail.com>
2022-04-07 23:10:42 -0300

    an error is throw if some proper is missing if counter on input component is able or some proper is invalid

--- src/components/DesignSystemElements/Input/index.tsx
+++ src/components/DesignSystemElements/Input/index.tsx
@@ -19,10 +19,26 @@ interface Props{
 
 export const Input:NextPage<Props> = ({ placeHolder, type="text", inputVariableState="", inputFunctionState, isCounterInput=false, maxNumberOfCharacters, charactersCounter }) => {
 
+  if(isCounterInput && maxNumberOfCharacters === undefined){
+    throw "prop 'isCounterInput' is true but prop 'maxNumberOfCharacters' not found";
+  }
+
+  if(maxNumberOfCharacters !== undefined && maxNumberOfCharacters < 1){
+    throw "prop 'maxNumberOfCharacters' most be greater than zero";
+  }
+
+  if(isCounterInput && charactersCounter === undefined){
+    throw "prop 'isCounterInput' is true but prop 'charactersCounter' not found";
+  }
+
+  if(charactersCounter !== undefined && charactersCounter < 0){
+    throw "prop 'charactersCounter' most be zero or greater";
+  }
+
   return(
     <S.Container>
       <S.Input placeholder={placeHolder} type={type} onChange={inputFunctionState} value={inputVariableState}/>
-      {isCounterInput && type=="number" &&(
+      {isCounterInput && type=="number" && (
         <S.CounterContainer>
           <S.NumberLimitCounter>{`${charactersCounter}/${maxNumberOfCharacters}`}</S.NumberLimitCounter>
         </S.CounterContainer>
