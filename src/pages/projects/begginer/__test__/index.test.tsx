115390dfde241c8ad9b74e2640d1e113d48de6b5
vitor-mat <vitormateusalves123@gmail.com>
2022-04-10 09:32:53 -0300

    fix test - describe(user interaction with input) it(input accept only six characters) - dec2bin

--- src/pages/projects/begginer/__test__/index.test.tsx
+++ src/pages/projects/begginer/__test__/index.test.tsx
@@ -13,11 +13,17 @@ describe("user interaction with input", () => {
     expect(input).toBeInTheDocument()
   })
 
-  it('should showw numbers that user type on input', () => {
+  it('should show numbers that user type on input', () => {
     const { debug } = render(<Dec2Bin />)
     const input = screen.getByPlaceholderText('Type your number')
     userEvent.type(input, '123456')
-    debug()
+    expect(input).toHaveValue('123456')
+  })
+
+  it('input accept only six characters', () => {
+    const { debug } = render(<Dec2Bin />)
+    const input = screen.getByPlaceholderText('Type your number')
+    userEvent.type(input, '123456789')
     expect(input).toHaveValue('123456')
   })
 
